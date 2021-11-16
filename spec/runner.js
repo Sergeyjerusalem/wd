de17f75ef367122147e2ebc2cf16fe830c598250
Sergeyjerusalem <90954618+Sergeyjerusalem@users.noreply.github.com>
2021-11-16 14:16:26 +0300

    Delete spec directory

--- spec/runner.js
+++
@@ -1,9 +0,0 @@
-let Jasmine = require('jasmine');
-let jasmine = new Jasmine;
-
-(async () => {
-    jasmine.loadConfigFile('G:/git/wd/spec/support/jasmine.json');
-    jasmine.configureDefaultReporter({ showColors: true });
-    jasmine.execute();
-
-})();
